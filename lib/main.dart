6c0a323ee825d6f050b79cc20b80ab6966c083f1
Yash-Gupta001 <gyash4963@gmail.com>
2025-02-24 14:48:25 +0530

    project structure defined

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:task_app/app/data/services/storage/services.dart';
 
-import 'pages/home.dart';
+import 'app/modules/home/view.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task app using getx',
       home: Home(),
     );
   }
