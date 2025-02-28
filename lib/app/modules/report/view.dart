b050b24b498ef46f4088b7719c264384581e4241
Yash-Gupta001 <gyash4963@gmail.com>
2025-02-28 18:53:48 +0530

    progressbar added for demo

--- lib/app/modules/report/view.dart
+++ lib/app/modules/report/view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class ReportPage extends StatelessWidget {
   const ReportPage({super.key});
@@ -7,10 +8,19 @@ class ReportPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          'report'
+          child: CircularStepProgressIndicator(
+        totalSteps: 100,
+        currentStep: 72,
+        selectedColor: Colors.yellow,
+        unselectedColor: Colors.lightBlue,
+        padding: 0,
+        width: 100,
+        child: Icon(
+          Icons.percent,
+          color: Colors.red,
+          size: 84,
         ),
-      ),
+      )),
     );
   }
 }
